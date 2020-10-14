feb765feba623103b48eee5d70e462c0a9a6991b
virendranishad <nitvirendra@gmail.com>
2020-10-14 14:15:59 +0530

    Updates

---
+++ precache-manifest.86f5e21508160b2b6eaa690f9c4505ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050dd6d71740985731c7c5b8ad61be35",
+    "url": "/Live-Score/index.html"
+  },
+  {
+    "revision": "ccbb08411f350bd983d6",
+    "url": "/Live-Score/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "f1d09af6d2bc650dd46c",
+    "url": "/Live-Score/static/js/2.d0341b1f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Live-Score/static/js/2.d0341b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccbb08411f350bd983d6",
+    "url": "/Live-Score/static/js/main.f559d75f.chunk.js"
+  },
+  {
+    "revision": "858f93c69dd91bcf56ab",
+    "url": "/Live-Score/static/js/runtime-main.ffcc0598.js"
+  },
+  {
+    "revision": "7c1eb6d6f51214ae103d95909bc01d7c",
+    "url": "/Live-Score/static/media/vs.7c1eb6d6.png"
+  }
+]);
